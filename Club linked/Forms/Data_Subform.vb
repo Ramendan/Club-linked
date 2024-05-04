4779a80462ecc878795602175d7e28508fcbc47c
Hassan <tootha22an@gmail.com>
2024-05-04 09:51:42 +0400

    Removed datasubform

--- Club linked/Forms/Data_Subform.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Data_Subform
-    Private Sub button5_Click(sender As Object, e As EventArgs) Handles button5.Click
-        Me.Close()
-    End Sub
-
-    Private Sub label1_Click(sender As Object, e As EventArgs) Handles label1.Click
-
-    End Sub
-End Class
