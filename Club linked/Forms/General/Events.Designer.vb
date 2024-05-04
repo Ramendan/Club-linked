ab7eb63216fb4be9bff6ba8626874adb27ba12e1
Hassan <tootha22an@gmail.com>
2024-05-04 09:25:06 +0400

    Added calendar to events

--- Club linked/Forms/General/Events.Designer.vb
+++ Club linked/Forms/General/Events.Designer.vb
@@ -27,12 +27,11 @@ Partial Class Events
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.txtSearch = New System.Windows.Forms.TextBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.btnNext = New System.Windows.Forms.Button()
-        Me.btnPrev = New System.Windows.Forms.Button()
-        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
@@ -67,68 +66,77 @@ Partial Class Events
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(191, 18)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.DataGridView1.Location = New System.Drawing.Point(258, 18)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(613, 354)
+        Me.DataGridView1.Size = New System.Drawing.Size(546, 354)
         Me.DataGridView1.TabIndex = 33
         '
         'txtSearch
         '
         Me.txtSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtSearch.Location = New System.Drawing.Point(191, 386)
-        Me.txtSearch.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtSearch.Location = New System.Drawing.Point(258, 386)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(2)
         Me.txtSearch.Name = "txtSearch"
-        Me.txtSearch.Size = New System.Drawing.Size(613, 31)
+        Me.txtSearch.Size = New System.Drawing.Size(546, 31)
         Me.txtSearch.TabIndex = 34
         Me.txtSearch.Text = "Enter your search here..."
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.MonthCalendar1)
         Me.GroupBox1.Controls.Add(Me.GroupBox2)
         Me.GroupBox1.Controls.Add(Me.txtSearch)
         Me.GroupBox1.Controls.Add(Me.DataGridView1)
-        Me.GroupBox1.Controls.Add(Me.btnNext)
-        Me.GroupBox1.Controls.Add(Me.btnPrev)
         Me.GroupBox1.Controls.Add(Me.btnSearch)
         Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(83, Byte), Integer))
         Me.GroupBox1.Location = New System.Drawing.Point(17, 61)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
         Me.GroupBox1.Size = New System.Drawing.Size(819, 447)
         Me.GroupBox1.TabIndex = 33
         Me.GroupBox1.TabStop = False
         '
-        'btnNext
+        'MonthCalendar1
         '
-        Me.btnNext.BackColor = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btnNext.FlatAppearance.BorderSize = 0
-        Me.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnNext.ForeColor = System.Drawing.Color.LightGray
-        Me.btnNext.Location = New System.Drawing.Point(20, 18)
-        Me.btnNext.Name = "btnNext"
-        Me.btnNext.Size = New System.Drawing.Size(150, 40)
-        Me.btnNext.TabIndex = 32
-        Me.btnNext.Text = "Next"
-        Me.btnNext.UseVisualStyleBackColor = False
+        Me.MonthCalendar1.Location = New System.Drawing.Point(20, 18)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 38
         '
-        'btnPrev
+        'GroupBox2
         '
-        Me.btnPrev.BackColor = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btnPrev.FlatAppearance.BorderSize = 0
-        Me.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnPrev.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnPrev.ForeColor = System.Drawing.Color.LightGray
-        Me.btnPrev.Location = New System.Drawing.Point(20, 75)
-        Me.btnPrev.Name = "btnPrev"
-        Me.btnPrev.Size = New System.Drawing.Size(150, 40)
-        Me.btnPrev.TabIndex = 31
-        Me.btnPrev.Text = "Prev"
-        Me.btnPrev.UseVisualStyleBackColor = False
+        Me.GroupBox2.Controls.Add(Me.CheckBox1)
+        Me.GroupBox2.Controls.Add(Me.CheckBox2)
+        Me.GroupBox2.Location = New System.Drawing.Point(20, 192)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(233, 180)
+        Me.GroupBox2.TabIndex = 37
+        Me.GroupBox2.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(16, 19)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(172, 24)
+        Me.CheckBox1.TabIndex = 35
+        Me.CheckBox1.Text = "Only from clubs im in"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox2.Location = New System.Drawing.Point(16, 49)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(147, 24)
+        Me.CheckBox2.TabIndex = 36
+        Me.CheckBox2.Text = "Hide past events"
+        Me.CheckBox2.UseVisualStyleBackColor = True
         '
         'btnSearch
         '
@@ -140,40 +148,10 @@ Partial Class Events
         Me.btnSearch.ForeColor = System.Drawing.Color.LightGray
         Me.btnSearch.Location = New System.Drawing.Point(20, 384)
         Me.btnSearch.Name = "btnSearch"
-        Me.btnSearch.Size = New System.Drawing.Size(150, 40)
+        Me.btnSearch.Size = New System.Drawing.Size(227, 40)
         Me.btnSearch.TabIndex = 29
         Me.btnSearch.Text = "Search for event"
         Me.btnSearch.UseVisualStyleBackColor = False
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(16, 19)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(122, 17)
-        Me.CheckBox1.TabIndex = 35
-        Me.CheckBox1.Text = "Only from clubs im in"
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(16, 42)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(106, 17)
-        Me.CheckBox2.TabIndex = 36
-        Me.CheckBox2.Text = "Hide past events"
-        Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'GroupBox2
-        '
-        Me.GroupBox2.Controls.Add(Me.CheckBox1)
-        Me.GroupBox2.Controls.Add(Me.CheckBox2)
-        Me.GroupBox2.Location = New System.Drawing.Point(20, 134)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(150, 74)
-        Me.GroupBox2.TabIndex = 37
-        Me.GroupBox2.TabStop = False
         '
         'Events
         '
@@ -184,7 +162,7 @@ Partial Class Events
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.button5)
         Me.Controls.Add(Me.label1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Events"
         Me.Text = "Events"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -202,10 +180,9 @@ Partial Class Events
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents txtSearch As TextBox
     Friend WithEvents GroupBox1 As GroupBox
-    Private WithEvents btnNext As Button
-    Private WithEvents btnPrev As Button
     Private WithEvents btnSearch As Button
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents MonthCalendar1 As MonthCalendar
 End Class
